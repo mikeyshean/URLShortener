6a24dbce29c736268572eaf47708cab64bb36ea5
Mikey Shean <mdshean2@gmail.com>
2015-08-12 12:36:33 -0400

    Create User migration and model.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = 'cc633c2fbef129f9d5fae06651964aaf547830e7fd2b277b5682c7b5592deb67b986406bf6178c81da468cd10fbe24919ab368e7c7ee4b7917834fbdd2d69d25'
